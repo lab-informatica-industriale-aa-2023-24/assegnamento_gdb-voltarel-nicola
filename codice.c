591f5c67964cd4e038c09be8106a4dc3be7162b8
Voltarel Nicola <nicola.voltarel@studenti.unipd.it>
2024-04-12 13:12:55 +0200

    modifiche fatte:
    1) nella funzione "ordina_dati" non era corretto il calcolo della lunghezza della array usando il sizeof
    
    2) nella funzione "inserisci" nel primo if c'era l'operatore di assegnazione invece di quello di comparazione
    
    3) nella funzione "inserisci" non veniva inserito il nuovo dato nel caso questo non fosse stato più piccolo degli altri
    
    ora l'output non è più composto da zeri, gli input già ordinati vengono stampati correttamente
    ma ci sono ancora problemi con input disordinati

--- codice.c
+++ codice.c
@@ -24,7 +24,7 @@ void fai_spazio(int posizione, int *vett, int lung)
 
 void inserisci(int nuovo_dato, int num_dati_ord, int *vett)
 { 
-	if (num_dati_ord = 0)  { // il vettore è vuoto, facile
+	if (num_dati_ord == 0)  { // il vettore è vuoto, facile
 		vett[0] = nuovo_dato;
 		return;
 	}
@@ -38,11 +38,12 @@ void inserisci(int nuovo_dato, int num_dati_ord, int *vett)
 			return;
 		}
 	}
+
+	vett[num_dati_ord] = nuovo_dato;
 }
 
-void ordina_dati(const int *dati_non_ordinati, int *dati_ordinati)
+void ordina_dati(const int *dati_non_ordinati, int *dati_ordinati, int num_dati)
 {
-	int num_dati = sizeof(dati_non_ordinati) / sizeof(dati_non_ordinati[0]);
 	for (int i = 0; i < num_dati; ++i)
 		inserisci(dati_non_ordinati[i], i, dati_ordinati);
 }
@@ -65,7 +66,7 @@ int main(int argc, char **argv)
 	int num_dati = 0;
 
 	estrai_dati(argc, argv, dati_input, &num_dati);
-	ordina_dati(dati_input, dati_ordinati);
+	ordina_dati(dati_input, dati_ordinati, num_dati);
 	stampa_vettore(dati_ordinati, num_dati);
 	return 0;
 }
